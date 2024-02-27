290b8cad861dd1f85c9a3f1b6fc0eea7d532bdd9
vambrosi <vambrosi@indiana.edu>
2024-02-27 09:19:19 -0800

    Add support for other functions

--- Embedded.jl
+++ Embedded.jl
@@ -22,6 +22,15 @@ md"""
 Viewer embedded in Pluto to explore dynamical systems.
 """
 
+# ╔═╡ 6ea317f3-3332-4485-a62a-f74d3a5eae01
+f(z, c) = z^2 + c
+
+# ╔═╡ c3bc70c0-fc7d-408c-a947-0c9a55e04537
+g(z, c) = z^2 + c/z^2
+
+# ╔═╡ db2c2bd5-3e0f-4e26-84f9-d1de720cb10c
+h(z, c) = (z^2 - c) / (z^2 - 1)
+
 # ╔═╡ 66d4ccf1-ac02-42a3-b02c-6992efb8a84b
 md"""
 ## Definitions
@@ -33,9 +42,9 @@ function continuous_iter(n::Integer, z::Complex)
 end
 
 # ╔═╡ ea71085e-4f4e-4ae7-a492-3852bf266d14
-function escape_time(z::Complex, c::Complex)
+function escape_time(f::Function, z::Complex, c::Complex)
 	for n in 1:512
-		z = z^2 + c
+		z = f(z, c)
 		if abs2(z) > 1000.0
 			return continuous_iter(n, z)
 		end
@@ -48,29 +57,47 @@ end
 to_range(raw_range, n) = range(minimum(raw_range), maximum(raw_range); length=n)
 
 # ╔═╡ 675bd95b-b86a-4479-81f2-b3d3e79f4dc6
-function mandel_slice!(grid, ix, x, yrange)
+function mandel_slice!(grid, f, ix, x, yrange)
 	@inbounds for (iy, y) in enumerate(yrange)
-		grid[iy, ix] = escape_time(0.0im, Complex(x,y))
+		grid[iy, ix] = escape_time(f, 0.0im, Complex(x,y))
+	end
+end
+
+# ╔═╡ 1861ac13-6087-4938-9705-f23ce940d7b4
+function julia_slice!(grid, f, ix, x, c, yrange)
+	@inbounds for (iy, y) in enumerate(yrange)
+		grid[iy, ix] = escape_time(f, Complex(x,y), c)
 	end
 end
 
 # ╔═╡ 4d34509f-b36b-42e4-9e68-8f2540eddeae
-function mandel_grid(xrange, yrange)
+function grid(f, c, xrange, yrange, is_mandel)
 	grid = Matrix{Float64}(undef, length(xrange), length(yrange))
 	futures = Vector{Task}(undef, length(xrange))
-	@inbounds for (ix, x) in enumerate(xrange)
-		futures[ix] = Threads.@spawn mandel_slice!(grid, ix, x, yrange)
+	if is_mandel
+		@inbounds for (ix, x) in enumerate(xrange)
+			futures[ix] = Threads.@spawn mandel_slice!(grid, f, ix, x, yrange)
+		end
+	else
+		@inbounds for (ix, x) in enumerate(xrange)
+			futures[ix] = Threads.@spawn julia_slice!(grid, f, ix, x, c, yrange)
+		end
 	end
 	wait.(futures)
 	return grid
 end
 
 # ╔═╡ 199f62a9-2b3c-43c5-a4f9-46d82eb38a9c
-function mandel()
+function view(
+	f::Function, center::ComplexF64, diam::Real, c::ComplexF64, is_mandel::Bool
+)
 	n = 650
-	x = to_range([-2.5, 1.5], n)
-	y = to_range([-2, 2], n)
-	z = mandel_grid(x,y)
+	xlo, xhi = real(center) - diam / 2, real(center) + diam / 2
+	ylo, yhi = imag(center) - diam / 2, imag(center) + diam / 2
+	
+	x = to_range([xlo, xhi], n)
+	y = to_range([ylo, yhi], n)
+	z = grid(f, c, x, y, is_mandel)
 	data = heatmap(;x,y,z,
 		colorscale=PlutoPlotly.colors.cyclical[:twilight],
 		zmin = 0.0, zmax = 1.0,
@@ -81,13 +108,13 @@ function mandel()
 		Layout(
 			margin = attr(t=0, b=0, l=0, r=0),
 			xaxis = attr(;
-				range = [-2.5, 1.5],
+				range = [xlo, xhi],
 				showgrid = false,
 				showticklabels = false,
 				zeroline = false,
 			),
 			yaxis = attr(;
-				range = [-2,2],
+				range = [ylo, yhi],
 				scaleanchor = "x",
 				scaleratio = 1,
 				showgrid = false,
@@ -98,7 +125,8 @@ function mandel()
 			height = n,
 			width = n,
 			dragmode = "pan",
-		)
+		),
+		config = PlotConfig(responsive=false, modeBarButtonsToRemove=["autoscale"])
 	)
 	add_plotly_listener!(p, "plotly_relayout", htl_js("async (e) => {
 		let xrange = PLOT._fullLayout.xaxis.range
@@ -107,10 +135,10 @@ function mandel()
 		let update = await PLOT.updated_data({x: xrange, y: yrange})
 		Plotly.restyle(PLOT, update, 0)
 	}"))
-	f(d) = let
+	new_grid(d) = let
 		x = range(d["x"]..., n) |> collect
 		y = range(d["y"]..., n) |> collect
-		z = mandel_grid(x, y) |> PlutoPlotly._preprocess
+		z = grid(f, c, x, y, is_mandel) |> PlutoPlotly._preprocess
 		out = (;
 			x = [x],
 			y = [y],
@@ -122,101 +150,42 @@ function mandel()
 	$p
 	<script>
 		const plt = currentScript.closest('pluto-cell').querySelector('.js-plotly-plot')
-		plt.updated_data = $(AbstractPlutoDingetjes.Display.with_js_link(f))
+		plt.updated_data = $(AbstractPlutoDingetjes.Display.with_js_link(new_grid))
 	</script>
 	""")
-end
-
-# ╔═╡ 1d93dadf-964b-4f20-904b-94e26ec59918
-mandel()
-
-# ╔═╡ 1861ac13-6087-4938-9705-f23ce940d7b4
-function julia_slice!(grid, ix, x, c, yrange)
-	@inbounds for (iy, y) in enumerate(yrange)
-		grid[iy, ix] = escape_time(Complex(x,y), c)
-	end
-end
-
-# ╔═╡ ee9a255d-152a-4727-9034-372a61947023
-function julia_grid(xrange, yrange, c)
-	grid = Matrix{Float64}(undef, length(xrange), length(yrange))
-	futures = Vector{Task}(undef, length(xrange))
-	@inbounds for (ix, x) in enumerate(xrange)
-		futures[ix] = Threads.@spawn julia_slice!(grid, ix, x, c, yrange)
-	end
-	wait.(futures)
-	return grid
 end
 
 # ╔═╡ 8ece44ee-a8c9-4a9a-a7cc-cbcf0fbe741b
-function julia(c)
-	n = 650
-	x = to_range([-2, 2], n)
-	y = to_range([-2, 2], n)
-	z = julia_grid(x,y, convert(ComplexF64, c))
-	data = heatmap(;x,y,z,
-		colorscale=PlutoPlotly.colors.cyclical[:twilight],
-		zmin = 0.0, zmax = 1.0,
-		showscale = false,
-	)
-	p = plot(
-		data, 
-		Layout(
-			margin = attr(t=0, b=0, l=0, r=0),
-			xaxis = attr(;
-				range = [-2, 2],
-				showgrid = false,
-				showticklabels = false,
-				zeroline = false,
-			),
-			yaxis = attr(;
-				range = [-2,2],
-				scaleanchor = "x",
-				scaleratio = 1,
-				showgrid = false,
-				showticklabels = false,
-				zeroline = false,
-			),
-			showlegend = false,
-			height = n,
-			width = n,
-			dragmode = "pan",
-		)
-	)
-	add_plotly_listener!(p, "plotly_relayout", htl_js("async (e) => {
-		let xrange = PLOT._fullLayout.xaxis.range
-		let yrange = PLOT._fullLayout.yaxis.range
-		if (PLOT.updated_data == undefined) {return}
-		let update = await PLOT.updated_data({x: xrange, y: yrange})
-		Plotly.restyle(PLOT, update, 0)
-	}"))
-	f(d) = let
-		x = range(d["x"]..., n) |> collect
-		y = range(d["y"]..., n) |> collect
-		z = julia_grid(x, y, convert(ComplexF64, c)) |> PlutoPlotly._preprocess
-		out = (;
-			x = [x],
-			y = [y],
-			z = [z],
-		)
-		out
-	end
-	return @htl("""
-	$p
-	<script>
-		const plt = currentScript.closest('pluto-cell').querySelector('.js-plotly-plot')
-		plt.updated_data = $(AbstractPlutoDingetjes.Display.with_js_link(f))
-	</script>
-	""")
-end
+julia(f; center=0.0im, diam=4.0, c=0.0im) = view(
+	f, convert(ComplexF64, center), diam, convert(ComplexF64, c), false
+)
 
 # ╔═╡ e7e56282-cc36-498a-8bd6-82bd1fa81cbd
-julia(1.0im)
+julia(f; c=1.0im)
+
+# ╔═╡ ac46ec26-c395-4aa0-bd6c-1e66fefea8d3
+julia(g; c=-0.05)
+
+# ╔═╡ 37edc9a2-e864-4963-959b-d7760184d0ba
+mandel(f; center=0.0im, diam=4.0, c=0.0im) = view(
+	f, convert(ComplexF64, center), diam, convert(ComplexF64, c), true
+)
+
+# ╔═╡ 1d93dadf-964b-4f20-904b-94e26ec59918
+mandel(f; center=-0.5)
+
+# ╔═╡ 3bdde31c-9fac-4475-824e-09371fba5df6
+mandel(h; diam=6.0)
 
 # ╔═╡ Cell order:
 # ╟─fe433fb7-db21-4f24-b786-246dbd6ad280
+# ╠═6ea317f3-3332-4485-a62a-f74d3a5eae01
 # ╠═1d93dadf-964b-4f20-904b-94e26ec59918
 # ╠═e7e56282-cc36-498a-8bd6-82bd1fa81cbd
+# ╠═c3bc70c0-fc7d-408c-a947-0c9a55e04537
+# ╠═ac46ec26-c395-4aa0-bd6c-1e66fefea8d3
+# ╠═db2c2bd5-3e0f-4e26-84f9-d1de720cb10c
+# ╠═3bdde31c-9fac-4475-824e-09371fba5df6
 # ╟─66d4ccf1-ac02-42a3-b02c-6992efb8a84b
 # ╟─7ca0bea0-d4e6-11ee-0acc-639605e39050
 # ╟─4b35056f-00aa-459d-93f0-dd8422288187
@@ -224,7 +193,7 @@ julia(1.0im)
 # ╟─498da509-562e-44cf-aaa0-77563d85e525
 # ╟─4d34509f-b36b-42e4-9e68-8f2540eddeae
 # ╟─675bd95b-b86a-4479-81f2-b3d3e79f4dc6
-# ╟─199f62a9-2b3c-43c5-a4f9-46d82eb38a9c
-# ╟─ee9a255d-152a-4727-9034-372a61947023
 # ╟─1861ac13-6087-4938-9705-f23ce940d7b4
+# ╟─199f62a9-2b3c-43c5-a4f9-46d82eb38a9c
 # ╟─8ece44ee-a8c9-4a9a-a7cc-cbcf0fbe741b
+# ╟─37edc9a2-e864-4963-959b-d7760184d0ba
