24a4e91c2b4d45334b10bec29df971947a731bb5
vambrosi <vambrosi@indiana.edu>
2024-02-27 19:24:14 -0800

    Add projective coloring method

--- Embedded.jl
+++ Embedded.jl
@@ -10,190 +10,66 @@ begin
 	Pkg.activate(@__DIR__)
 	Pkg.instantiate()
 	
-	using PlutoPlotly, HypertextLiteral, AbstractPlutoDingetjes
+	include("./src/Mandel.jl")
+	using .Mandel
 
-	md"Instatiates project" 
+	mandel = Mandel.mandel
+	julia = Mandel.julia
 end
 
 # ╔═╡ fe433fb7-db21-4f24-b786-246dbd6ad280
 md"""
 # Embedded.jl
 
-Viewer embedded in Pluto to explore dynamical systems.
+Example file to show how to use the embedded user interface. Copy this file and change the functions to test it out. The command `git pull` will automatically update functionalities on the copy.
+"""
+
+# ╔═╡ d9d42585-e924-4175-9fe1-d7aa30baace9
+md"""
+First, define a 1-parameter family of functions to iterate as the following example:
 """
 
 # ╔═╡ 6ea317f3-3332-4485-a62a-f74d3a5eae01
 f(z, c) = z^2 + c
 
-# ╔═╡ c3bc70c0-fc7d-408c-a947-0c9a55e04537
-g(z, c) = z^2 + c/z^2
-
-# ╔═╡ db2c2bd5-3e0f-4e26-84f9-d1de720cb10c
-h(z, c) = (z^2 - c) / (z^2 - 1)
-
-# ╔═╡ 66d4ccf1-ac02-42a3-b02c-6992efb8a84b
+# ╔═╡ 23b6ed24-13e1-41ee-98a7-66bd0e2a8a4f
 md"""
-## Definitions
+Then, call `julia(f)` or `mandel(f)` to plot the respective sets.
 """
-
-# ╔═╡ 4b35056f-00aa-459d-93f0-dd8422288187
-function continuous_iter(n::Integer, z::Complex)
-	return mod((n + 1.0 - log2(log(abs(z)))) / 64.0, 1.0)
-end
-
-# ╔═╡ ea71085e-4f4e-4ae7-a492-3852bf266d14
-function escape_time(f::Function, z::Complex, c::Complex)
-	for n in 1:512
-		z = f(z, c)
-		if abs2(z) > 1000.0
-			return continuous_iter(n, z)
-		end
-	end
-
-	return 0.5
-end
-
-# ╔═╡ 498da509-562e-44cf-aaa0-77563d85e525
-to_range(raw_range, n) = range(minimum(raw_range), maximum(raw_range); length=n)
-
-# ╔═╡ 675bd95b-b86a-4479-81f2-b3d3e79f4dc6
-function mandel_slice!(grid, f, ix, x, yrange)
-	@inbounds for (iy, y) in enumerate(yrange)
-		grid[iy, ix] = escape_time(f, 0.0im, Complex(x,y))
-	end
-end
-
-# ╔═╡ 1861ac13-6087-4938-9705-f23ce940d7b4
-function julia_slice!(grid, f, ix, x, c, yrange)
-	@inbounds for (iy, y) in enumerate(yrange)
-		grid[iy, ix] = escape_time(f, Complex(x,y), c)
-	end
-end
-
-# ╔═╡ 4d34509f-b36b-42e4-9e68-8f2540eddeae
-function grid(f, c, xrange, yrange, is_mandel)
-	grid = Matrix{Float64}(undef, length(xrange), length(yrange))
-	futures = Vector{Task}(undef, length(xrange))
-	if is_mandel
-		@inbounds for (ix, x) in enumerate(xrange)
-			futures[ix] = Threads.@spawn mandel_slice!(grid, f, ix, x, yrange)
-		end
-	else
-		@inbounds for (ix, x) in enumerate(xrange)
-			futures[ix] = Threads.@spawn julia_slice!(grid, f, ix, x, c, yrange)
-		end
-	end
-	wait.(futures)
-	return grid
-end
-
-# ╔═╡ 199f62a9-2b3c-43c5-a4f9-46d82eb38a9c
-function view(
-	f::Function, center::ComplexF64, diam::Real, c::ComplexF64, is_mandel::Bool
-)
-	n = 650
-	xlo, xhi = real(center) - diam / 2, real(center) + diam / 2
-	ylo, yhi = imag(center) - diam / 2, imag(center) + diam / 2
-	
-	x = to_range([xlo, xhi], n)
-	y = to_range([ylo, yhi], n)
-	z = grid(f, c, x, y, is_mandel)
-	data = heatmap(;x,y,z,
-		colorscale=PlutoPlotly.colors.cyclical[:twilight],
-		zmin = 0.0, zmax = 1.0,
-		showscale = false,
-	)
-	p = plot(
-		data, 
-		Layout(
-			margin = attr(t=0, b=0, l=0, r=0),
-			xaxis = attr(;
-				range = [xlo, xhi],
-				showgrid = false,
-				showticklabels = false,
-				zeroline = false,
-			),
-			yaxis = attr(;
-				range = [ylo, yhi],
-				scaleanchor = "x",
-				scaleratio = 1,
-				showgrid = false,
-				showticklabels = false,
-				zeroline = false,
-			),
-			showlegend = false,
-			height = n,
-			width = n,
-			dragmode = "pan",
-		),
-		config = PlotConfig(responsive=false, modeBarButtonsToRemove=["autoscale"])
-	)
-	add_plotly_listener!(p, "plotly_relayout", htl_js("async (e) => {
-		let xrange = PLOT._fullLayout.xaxis.range
-		let yrange = PLOT._fullLayout.yaxis.range
-		if (PLOT.updated_data == undefined) {return}
-		let update = await PLOT.updated_data({x: xrange, y: yrange})
-		Plotly.restyle(PLOT, update, 0)
-	}"))
-	new_grid(d) = let
-		x = range(d["x"]..., n) |> collect
-		y = range(d["y"]..., n) |> collect
-		z = grid(f, c, x, y, is_mandel) |> PlutoPlotly._preprocess
-		out = (;
-			x = [x],
-			y = [y],
-			z = [z],
-		)
-		out
-	end
-	return @htl("""
-	$p
-	<script>
-		const plt = currentScript.closest('pluto-cell').querySelector('.js-plotly-plot')
-		plt.updated_data = $(AbstractPlutoDingetjes.Display.with_js_link(new_grid))
-	</script>
-	""")
-end
-
-# ╔═╡ 8ece44ee-a8c9-4a9a-a7cc-cbcf0fbe741b
-julia(f; center=0.0im, diam=4.0, c=0.0im) = view(
-	f, convert(ComplexF64, center), diam, convert(ComplexF64, c), false
-)
-
-# ╔═╡ e7e56282-cc36-498a-8bd6-82bd1fa81cbd
-julia(f; c=1.0im)
-
-# ╔═╡ ac46ec26-c395-4aa0-bd6c-1e66fefea8d3
-julia(g; c=-0.05)
-
-# ╔═╡ 37edc9a2-e864-4963-959b-d7760184d0ba
-mandel(f; center=0.0im, diam=4.0, c=0.0im) = view(
-	f, convert(ComplexF64, center), diam, convert(ComplexF64, c), true
-)
 
 # ╔═╡ 1d93dadf-964b-4f20-904b-94e26ec59918
 mandel(f; center=-0.5)
 
-# ╔═╡ 3bdde31c-9fac-4475-824e-09371fba5df6
-mandel(h; diam=6.0)
+# ╔═╡ c80bcd93-4721-4507-9c17-00b8972090ef
+julia(f; c=-0.12711+0.75706im)
+
+# ╔═╡ e7e56282-cc36-498a-8bd6-82bd1fa81cbd
+julia(f; c=-0.12711+0.75706im, coloring_alg=:projective_convergence)
+
+# ╔═╡ 13d8745b-fb7d-481f-8f5d-67b72c806da2
+md"""
+You can choose the initial center, initial diameter, and the parameter of the Julia set by choosing `center`, `diam`, and `c`. 
+
+The variable `coloring_alg` chooses the coloring algorithm for the plot. The options are:
+- `:escape_time` 
+- `:projective_convergence`.
+"""
+
+# ╔═╡ 66d4ccf1-ac02-42a3-b02c-6992efb8a84b
+md"""
+## Instantiating Environment
+
+This section imports the relevant functions.
+"""
 
 # ╔═╡ Cell order:
 # ╟─fe433fb7-db21-4f24-b786-246dbd6ad280
+# ╟─d9d42585-e924-4175-9fe1-d7aa30baace9
 # ╠═6ea317f3-3332-4485-a62a-f74d3a5eae01
+# ╟─23b6ed24-13e1-41ee-98a7-66bd0e2a8a4f
 # ╠═1d93dadf-964b-4f20-904b-94e26ec59918
+# ╠═c80bcd93-4721-4507-9c17-00b8972090ef
 # ╠═e7e56282-cc36-498a-8bd6-82bd1fa81cbd
-# ╠═c3bc70c0-fc7d-408c-a947-0c9a55e04537
-# ╠═ac46ec26-c395-4aa0-bd6c-1e66fefea8d3
-# ╠═db2c2bd5-3e0f-4e26-84f9-d1de720cb10c
-# ╠═3bdde31c-9fac-4475-824e-09371fba5df6
+# ╟─13d8745b-fb7d-481f-8f5d-67b72c806da2
 # ╟─66d4ccf1-ac02-42a3-b02c-6992efb8a84b
-# ╟─7ca0bea0-d4e6-11ee-0acc-639605e39050
-# ╟─4b35056f-00aa-459d-93f0-dd8422288187
-# ╟─ea71085e-4f4e-4ae7-a492-3852bf266d14
-# ╟─498da509-562e-44cf-aaa0-77563d85e525
-# ╟─4d34509f-b36b-42e4-9e68-8f2540eddeae
-# ╟─675bd95b-b86a-4479-81f2-b3d3e79f4dc6
-# ╟─1861ac13-6087-4938-9705-f23ce940d7b4
-# ╟─199f62a9-2b3c-43c5-a4f9-46d82eb38a9c
-# ╟─8ece44ee-a8c9-4a9a-a7cc-cbcf0fbe741b
-# ╟─37edc9a2-e864-4963-959b-d7760184d0ba
+# ╠═7ca0bea0-d4e6-11ee-0acc-639605e39050
---
+++ src/Mandel.jl
@@ -0,0 +1,278 @@
+module Mandel
+
+export mandel, julia
+
+using StaticArraysCore, LinearAlgebra, Symbolics, PlutoPlotly, HypertextLiteral, AbstractPlutoDingetjes
+
+# --------------------------------------------------------------------------- #
+# Complex Projective Line
+# --------------------------------------------------------------------------- #
+
+const Point = MVector{2, ComplexF64}
+
+function distance(pt1::Point, pt2::Point)
+	return norm(pt1[1] * pt2[2] - pt1[2] * pt2[1])
+end
+
+function projectivize(f::Function)
+	@variables u, v, c
+	frac = simplify(f(u / v, c))
+
+	n, d = Symbolics.arguments(Symbolics.value(frac))
+	fu = eval(build_function(n, u, v, c, expression=Val{false}))
+	fv = eval(build_function(d, u, v, c, expression=Val{false}))
+
+	return (pt, param) ->
+		normalize(Point(fu(pt[1], pt[2], param), fv(pt[1], pt[2], param)))
+end
+
+# --------------------------------------------------------------------------- #
+# Coloring Algorithms
+# --------------------------------------------------------------------------- #
+
+const DEFAULT_COLOR::Float64 = 0.5
+
+function continuous_iter(n::Integer, z::Complex)
+	return mod((n + 1.0 - log2(log(abs(z)))) / 64.0, 1.0)
+end
+
+function escape_time(f::Function, z::Complex, c::Complex)
+    for n in 1:512
+		z = f(z, c)
+		if abs2(z) > 1000.0
+			return continuous_iter(n, z)
+		end
+	end
+
+	return DEFAULT_COLOR
+end
+
+function repeated_iterate(f::Function, pt::Point, c::Number, ε::Real, max_iter::Integer)
+	slow = copy(pt)
+	fast = copy(pt)
+
+	iteration = 1
+	while iteration <= max_iter
+		slow = f(slow, c)
+		fast = f(f(fast, c), c)
+
+		if distance(fast, slow) <= ε
+			break
+		end
+
+		iteration += 1
+	end
+
+	return iteration, slow, fast
+end
+
+function near_again(f::Function, pt1::Point, pt2::Point, c::Number, ε::Real, max_period::Integer)
+	pt = copy(pt1)
+
+	iteration = 1
+	while iteration <= max_period
+		pt = f(pt, c)
+
+		if distance(pt, pt2) <= ε
+			break
+		end
+
+		iteration += 1
+	end
+
+	return iteration, pt1, pt2
+end
+
+function iterate_until_close(f::Function, pt1::Point, pt2::Point, c::Number, ε::Real, max_iter::Integer)
+	iteration = 0
+	while iteration <= max_iter
+		if distance(pt1, pt2) <= ε
+			break
+		end
+
+		pt1 = f(pt1, c)
+		pt2 = f(pt2, c)
+		iteration += 1
+	end
+
+	return iteration
+end
+
+function projective_convergence(f_proj::Function, z::Complex, c::Complex)
+	ε = 1e-4
+	max_iter = 200
+	pt = Point(z, 1)
+
+	time_diff, slow, fast = repeated_iterate(f_proj, pt, c, ε, max_iter)
+	time_diff > max_iter && return DEFAULT_COLOR
+
+	period, slow, fast = near_again(f_proj, slow, fast, c, ε, time_diff)
+	period > time_diff && return DEFAULT_COLOR
+
+	preperiod = iterate_until_close(f_proj, pt, fast, c, ε, max_iter)
+	preperiod > max_iter && return DEFAULT_COLOR
+
+	return mod((preperiod / period) / 64.0, 1.0)
+end
+
+algorithms = Dict(:escape_time => escape_time, :projective_convergence => projective_convergence)
+
+# --------------------------------------------------------------------------- #
+# Grid calculations
+# --------------------------------------------------------------------------- #
+
+to_range(raw_range, n) = range(minimum(raw_range), maximum(raw_range); length=n)
+
+function grid(f, c, xrange, yrange, is_mandel, algorithm)
+	grid = Matrix{Float64}(undef, length(xrange), length(yrange))
+	futures = Vector{Task}(undef, length(xrange))
+
+	if is_mandel
+		@inbounds for (ix, x) in enumerate(xrange)
+			futures[ix] = Threads.@spawn mandel_slice!(grid, f, ix, x, yrange, algorithm)
+		end
+	else
+		@inbounds for (ix, x) in enumerate(xrange)
+			futures[ix] = Threads.@spawn julia_slice!(grid, f, ix, x, c, yrange, algorithm)
+		end
+	end
+	wait.(futures)
+	return grid
+end
+
+function mandel_slice!(grid, f, ix, x, yrange, algorithm)
+	@inbounds for (iy, y) in enumerate(yrange)
+		grid[iy, ix] = algorithm(f, 0.0im, Complex(x,y))
+	end
+end
+
+function julia_slice!(grid, f, ix, x, c, yrange, algorithm)
+	@inbounds for (iy, y) in enumerate(yrange)
+		grid[iy, ix] = algorithm(f, Complex(x,y), c)
+	end
+end
+
+function view(
+	f::Function, center::ComplexF64, diam::Real, c::ComplexF64, is_mandel::Bool, coloring_alg::Symbol
+)
+	n = 650
+	xlo, xhi = real(center) - diam / 2, real(center) + diam / 2
+	ylo, yhi = imag(center) - diam / 2, imag(center) + diam / 2
+
+	algorithm = algorithms[coloring_alg]
+
+	# Uses projective version of the function if the algorithm tests convergence on the sphere.
+	g = coloring_alg == :projective_convergence ? projectivize(f) : f
+
+	x = to_range([xlo, xhi], n)
+	y = to_range([ylo, yhi], n)
+	z = grid(g, c, x, y, is_mandel, algorithm)
+
+	data = heatmap(;x,y,z,
+		colorscale=PlutoPlotly.colors.cyclical[:twilight],
+		zmin = 0.0, zmax = 1.0,
+		showscale = false,
+		name = is_mandel ? "Mandel" : "Julia",
+		hoverinfo = "skip",
+	)
+
+	points = scatter(x=[], y=[], mode="markers+lines", name="Tracker")
+
+	p = plot(
+		[data, points],
+		Layout(
+			margin = attr(t=0, b=0, l=0, r=0),
+			xaxis = attr(;
+				range = [xlo, xhi],
+				showgrid = false,
+				showticklabels = false,
+				zeroline = false,
+			),
+			yaxis = attr(;
+				range = [ylo, yhi],
+				scaleanchor = "x",
+				scaleratio = 1,
+				showgrid = false,
+				showticklabels = false,
+				zeroline = false,
+			),
+			showlegend = false,
+			height = n,
+			width = n,
+			dragmode = "pan",
+		),
+		config = PlotConfig(
+			responsive=false,
+			modeBarButtonsToRemove=["autoscale", "select2d", "lasso2d"],
+		),
+	)
+
+	add_plotly_listener!(p, "plotly_relayout", htl_js("async (e) => {
+		let xrange = PLOT._fullLayout.xaxis.range
+		let yrange = PLOT._fullLayout.yaxis.range
+		if (PLOT.updated_data == undefined) {return}
+		let update = await PLOT.updated_data({x: xrange, y: yrange})
+		Plotly.restyle(PLOT, update, 0)
+	}"))
+
+	new_grid(d) = let
+		x = range(d["x"]..., n) |> collect
+		y = range(d["y"]..., n) |> collect
+		z = grid(g, c, x, y, is_mandel, algorithm) |> PlutoPlotly._preprocess
+		out = (;
+			x = [x],
+			y = [y],
+			z = [z],
+		)
+		out
+	end
+
+	add_plotly_listener!(p, "plotly_click", htl_js("async (e) => {
+		console.log('click')
+	}"))
+
+	orbit_fixed(d) = let
+		v = orbit(f, complex(d["x"], d["y"]), c, is_mandel ? 1 : 1)
+		(; x = real(v), y = imag(v))
+	end
+
+	return @htl("""
+	$p
+	<script>
+		const plt = currentScript.closest('pluto-cell').querySelector('.js-plotly-plot')
+		plt.updated_data = $(AbstractPlutoDingetjes.Display.with_js_link(new_grid))
+		plt.orbit = $(AbstractPlutoDingetjes.Display.with_js_link(orbit_fixed))
+	</script>
+	""")
+end
+
+julia(f; center=0.0im, diam=4.0, c=0.0im, coloring_alg=:escape_time) = view(
+	f, convert(ComplexF64, center), convert(Float64, diam), convert(ComplexF64, c), false, coloring_alg
+)
+
+mandel(f; center=0.0im, diam=4.0, c=0.0im, coloring_alg=:escape_time) = view(
+	f, convert(ComplexF64, center), convert(Float64, diam), convert(ComplexF64, c), true, coloring_alg
+)
+
+# --------------------------------------------------------------------------- #
+# Other Functions
+# --------------------------------------------------------------------------- #
+
+function orbit(f::Function, z::Complex, c::Complex, n::Integer)
+	v = Vector{ComplexF64}(undef, n)
+	for i in eachindex(v)
+		v[i] = z
+		z = f(z, c)
+	end
+	return v
+end
+
+# --------------------------------------------------------------------------- #
+# Precompiling
+# --------------------------------------------------------------------------- #
+precompile(distance, (Point, Point))
+precompile(escape_time, (Function, ComplexF64, ComplexF64))
+precompile(projective_convergence, (Function, ComplexF64, ComplexF64))
+precompile(view, (Function, ComplexF64, Float64, ComplexF64, Bool, Symbol))
+
+end
