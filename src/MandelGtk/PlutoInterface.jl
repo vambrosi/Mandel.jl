7efc9b47373b75c72dd0379132d2f4fcd351a518
Vinicius Ambrosi <viniciusambrosi@gmail.com>
2025-11-07 11:04:33 -0800

    Simplify Pluto Interface

--- src/MandelGtk/PlutoInterface.jl
+++ src/MandelGtk/PlutoInterface.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.46
+# v0.20.18
 
 using Markdown
 using InteractiveUtils
@@ -10,17 +10,15 @@ begin
     Pkg.activate(Base.current_project())
 	Pkg.instantiate()
 
-    include("./src/MandelGTK.jl")
-    using .MandelGTK
-
-    viewer = MandelGTK.viewer
+    include("./src/MandelGtk.jl")
+    using .MandelGtk
 end
 
 # ╔═╡ 1cb7de2e-39d0-4e30-a668-f01281b35682
 f(z,c) = z^2 + c
 
 # ╔═╡ 174b726c-5c63-423b-b07f-21c8fcebf01a
-viewer(f, 1.0im)
+Viewer(f, mandel_center=-0.5)
 
 # ╔═╡ Cell order:
 # ╠═3a8b34f3-e4b3-4fd3-b94c-900b62e7dcdc
